055e70af3fd2584982bf21a2d99bfe64229b209d
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2024-09-06 22:37:40 +0630

    Redirect home timeline to public

--- app/controllers/api/v1/timelines/home_controller.rb
+++ app/controllers/api/v1/timelines/home_controller.rb
@@ -1,35 +1,77 @@
 # frozen_string_literal: true
 
 class Api::V1::Timelines::HomeController < Api::V1::Timelines::BaseController
-  before_action -> { doorkeeper_authorize! :read, :'read:statuses' }, only: [:show]
-  before_action :require_user!, only: [:show]
+  # before_action -> { doorkeeper_authorize! :read, :'read:statuses' }, only: [:show]
+  # before_action :require_user!, only: [:show]
 
-  PERMITTED_PARAMS = %i(local limit).freeze
+  # PERMITTED_PARAMS = %i(local limit).freeze
+
+  # def show
+  #   with_read_replica do
+  #     @statuses = load_statuses
+  #     @relationships = StatusRelationshipsPresenter.new(@statuses, current_user&.account_id)
+  #   end
+
+  #   render json: @statuses,
+  #          each_serializer: REST::StatusSerializer,
+  #          relationships: @relationships,
+  #          status: account_home_feed.regenerating? ? 206 : 200
+  # end
+
+  # private
+
+  # def load_statuses
+  #   preloaded_home_statuses
+  # end
+
+  # def preloaded_home_statuses
+  #   preload_collection home_statuses, Status
+  # end
+
+  # def home_statuses
+  #   account_home_feed.get(
+  #     limit_param(DEFAULT_STATUSES_LIMIT),
+  #     params[:max_id],
+  #     params[:since_id],
+  #     params[:min_id]
+  #   )
+  # end
+
+  # def account_home_feed
+  #   HomeFeed.new(current_account)
+  # end
+
+  # def next_path
+  #   api_v1_timelines_home_url next_path_params
+  # end
+
+  # def prev_path
+  #   api_v1_timelines_home_url prev_path_params
+  # end
+
+  before_action -> { authorize_if_got_token! :read, :'read:statuses' }
+
+  PERMITTED_PARAMS = %i(local remote limit only_media).freeze
 
   def show
-    with_read_replica do
-      @statuses = load_statuses
-      @relationships = StatusRelationshipsPresenter.new(@statuses, current_user&.account_id)
-    end
-
-    render json: @statuses,
-           each_serializer: REST::StatusSerializer,
-           relationships: @relationships,
-           status: account_home_feed.regenerating? ? 206 : 200
+    params.merge!(remote: false, only_media: false)
+    cache_if_unauthenticated!
+    @statuses = load_statuses
+    render json: @statuses, each_serializer: REST::StatusSerializer, relationships: StatusRelationshipsPresenter.new(@statuses, current_user&.account_id)
   end
 
   private
 
   def load_statuses
-    preloaded_home_statuses
+    preloaded_public_statuses_page
   end
 
-  def preloaded_home_statuses
-    preload_collection home_statuses, Status
+  def preloaded_public_statuses_page
+    preload_collection(public_statuses, Status)
   end
 
-  def home_statuses
-    account_home_feed.get(
+  def public_statuses
+    public_feed.get(
       limit_param(DEFAULT_STATUSES_LIMIT),
       params[:max_id],
       params[:since_id],
@@ -37,15 +79,20 @@ class Api::V1::Timelines::HomeController < Api::V1::Timelines::BaseController
     )
   end
 
-  def account_home_feed
-    HomeFeed.new(current_account)
+  def public_feed
+    PublicFeed.new(
+      current_account,
+      local: truthy_param?(:local),
+      remote: truthy_param?(:remote),
+      only_media: truthy_param?(:only_media)
+    )
   end
 
   def next_path
-    api_v1_timelines_home_url next_path_params
+    api_v1_timelines_public_url next_path_params
   end
 
   def prev_path
-    api_v1_timelines_home_url prev_path_params
+    api_v1_timelines_public_url prev_path_params
   end
 end
