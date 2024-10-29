198603f7d44bd1e61847883be151b7e5702aa134
SithuBo <sithubo.stb97@gmail.com>
2024-10-29 13:46:59 +0700

    [add] render env values

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -215,6 +215,9 @@ module ApplicationHelper
 
     state_params[:owner] = Account.local.without_suspended.without_internal.first if single_user_mode?
 
+    state_params[:header_image] = ENV.fetch('HEADER_IMAGE', nil)
+    state_params[:custom_link] = ENV.fetch('LINKS', nil)
+
     json = ActiveModelSerializers::SerializableResource.new(InitialStatePresenter.new(state_params), serializer: InitialStateSerializer).to_json
     # rubocop:disable Rails/OutputSafety
     content_tag(:script, json_escape(json).html_safe, id: 'initial-state', type: 'application/json')
