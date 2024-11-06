5777e03a940c7a823bea123815a5d6e93afff0bd
SithuBo <sithubo.stb97@gmail.com>
2024-11-06 15:49:54 +0700

    [remove] s3 url from csp

--- app/lib/content_security_policy.rb
+++ app/lib/content_security_policy.rb
@@ -10,7 +10,7 @@ class ContentSecurityPolicy
   end
 
   def media_hosts
-    [assets_host, cdn_host_value, paperclip_root_url, 'https://s3-eu-west-2.amazonaws.com/patchwork-prod'].compact
+    [assets_host, cdn_host_value, paperclip_root_url].compact
   end
 
   private
