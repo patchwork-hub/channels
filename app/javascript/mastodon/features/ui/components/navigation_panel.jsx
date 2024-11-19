fb40fb3e9aa579547b3cf8fa172c055378f794a8
Chan Nyein Tun <29679269+channyeintun@users.noreply.github.com>
2024-11-19 23:54:33 +0700

    fix: update logo size for news channel

--- app/javascript/mastodon/features/ui/components/navigation_panel.jsx
+++ app/javascript/mastodon/features/ui/components/navigation_panel.jsx
@@ -132,7 +132,7 @@ class NavigationPanel extends Component {
       <div className='navigation-panel'>
         <div className='navigation-panel__logo' style={{ paddingInline:16 }}>
           <Link to='/' className='column-link column-link--logo'>
-            {(subdomain==='news') ? <img width='60px' src='./temp-images/newsmast.png' alt='news logo' />:subdomain==='informationtechnology'?<img width='150px' alt='information technology logo' src='./temp-images/binarylab.png' />:channel_display_name}
+            {(subdomain==='news') ? <img width='175px' src='./temp-images/newsmast.png' alt='news logo' />:subdomain==='informationtechnology'?<img width='150px' alt='information technology logo' src='./temp-images/binarylab.png' />:channel_display_name}
           </Link>
         </div>
 
