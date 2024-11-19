12930c0d510cd63a84626c4f9ea0556c4cd98fee
SithuBo <sithubo.stb97@gmail.com>
2024-11-19 18:24:29 +0700

    [add] disable noti when reblog a status from community admin

--- app/services/reblog_request_service.rb
+++ app/services/reblog_request_service.rb
@@ -3,24 +3,24 @@
 class ReblogRequestService < BaseService
   require 'net/http'
 
-  def call(access_token, status_id)
+  def call(access_token, status_id, disable_noti: true)
     url = Rails.env.development? ? URI("http://localhost:3000/api/v1/statuses/#{status_id}/reblog") : URI("https://channel.org/api/v1/statuses/#{status_id}/reblog")
 
     req = Net::HTTP::Post.new(url)
     req.content_type = 'application/json'
     req['Authorization'] = "Bearer #{access_token}"
+    req.body = { disable_noti: disable_noti }.to_json
 
     response = Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == 'https') do |http|
       http.request(req)
     end
 
     case response
-
     when Net::HTTPSuccess
-    then
       JSON.parse(response.body)
     else
       raise "Reblog creation failed: #{response.body}"
     end
   end
+
 end
