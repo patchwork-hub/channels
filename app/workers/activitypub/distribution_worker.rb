437cd7d3874badcc205d8271ab94e0f2fb626fb4
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2024-11-26 14:33:39 +0630

    Mute notification only reblog

--- app/workers/activitypub/distribution_worker.rb
+++ app/workers/activitypub/distribution_worker.rb
@@ -16,7 +16,11 @@ class ActivityPub::DistributionWorker < ActivityPub::RawDistributionWorker
 
   def inboxes
     @inboxes ||= StatusReachFinder.new(@status).inboxes
-    @inboxes.delete("https://#{@status.reblog.account&.domain}/inbox") unless @inboxes.empty? && @status.reblog.account&.domain.nil?
+    if @status.reblog?
+      community_account_ids = User.joins(:role).where(user_roles: { name: 'community-admin' }).pluck(:account_id)
+      domain = @status.reblog.account&.domain
+      @inboxes.delete("https://#{domain}/inbox") if domain && !@inboxes.empty? && community_account_ids.include?(@status.account_id)
+    end
     @inboxes
   end
 
