489875dd0ac4174d63ac1442c0c26a2445fdb1f4
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2024-11-29 15:59:10 +0630

    Modify forget password

--- app/mailers/custom_passwords_mailer.rb
+++ app/mailers/custom_passwords_mailer.rb
@@ -2,11 +2,10 @@
 
 class CustomPasswordsMailer < ApplicationMailer
   layout 'email'
-  default from: %{Newsmast <#{ENV['SMTP_FROM_ADDRESS']}>}
+  default from: %{Patchwork <#{ENV['SMTP_FROM_ADDRESS']}>}
 
   def reset_password_confirmation
     @user = params[:user]
-    @web = params[:web]
     if @user.present?
       @subject = 'Reset your password'
       mail(to: @user.email, subject: @subject)
