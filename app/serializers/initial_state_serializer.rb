16d69ef0250f33388129e0a1ccafc12095cfd49e
SithuBo <sithubo.stb97@gmail.com>
2024-12-09 16:12:46 +0700

    [add] logo image for sub channel

--- app/serializers/initial_state_serializer.rb
+++ app/serializers/initial_state_serializer.rb
@@ -6,7 +6,7 @@ class InitialStateSerializer < ActiveModel::Serializer
   attributes :meta, :compose, :accounts,
              :media_attachments, :settings,
              :languages,
-             :header_image, :custom_links, :channel_display_name
+             :header_image, :custom_links, :channel_display_name, logo_image
 
   attribute :critical_updates_pending, if: -> { object&.role&.can?(:view_devops) && SoftwareUpdate.check_enabled? }
 
@@ -87,6 +87,10 @@ class InitialStateSerializer < ActiveModel::Serializer
 
   def header_image
     ENV.fetch('HEADER_IMAGE', nil)
+  end
+
+  def logo_image
+    ENV.fetch('LOGO_IMAGE', nil)
   end
 
   def custom_links
