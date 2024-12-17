841a64b6c2127a07ba4ea6caad0be08d01eddd3b
Chan Nyein Tun <29679269+channyeintun@users.noreply.github.com>
2024-12-17 15:52:05 +0700

    fix(logo image): reduced logo image size to 175px

--- app/javascript/mastodon/features/ui/components/navigations.jsx
+++ app/javascript/mastodon/features/ui/components/navigations.jsx
@@ -32,7 +32,7 @@ const Navigations = () => {
     <aside className='navigation-panel sidebar'>
       <div className='navigation-panel__logo' style={{ paddingInline:16 }}>
         <Link to='/' className='nav-header'>
-          {(subdomain==='news') ? <img width='175px' src='./temp-images/newsmast.png' alt='news logo' />:subdomain==='informationtechnology'?<img width='150px' alt='information technology logo' src='./temp-images/binarylab.png' />:logo_image?<img src={logo_image} width={200} alt='channel logo' />:channel_display_name}
+          {(subdomain==='news') ? <img width='175px' src='./temp-images/newsmast.png' alt='news logo' />:subdomain==='informationtechnology'?<img width='150px' alt='information technology logo' src='./temp-images/binarylab.png' />:logo_image?<img src={logo_image} width={175} style={{ aspectRatio:'36 / 10'}} alt='channel logo' />:channel_display_name}
         </Link>
       </div>
       <div className='nav-links'>
