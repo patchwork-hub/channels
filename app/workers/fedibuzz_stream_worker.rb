5b635241e6c34f1633f687ef559f0e89f67672c3
SithuBo <sithubo.stb97@gmail.com>
2025-01-06 13:59:09 +0700

    Restrict Fedibuzz streaming to allowed domain for enhanced security

--- app/workers/fedibuzz_stream_worker.rb
+++ app/workers/fedibuzz_stream_worker.rb
@@ -1,9 +1,16 @@
 class FedibuzzStreamWorker
   include Sidekiq::Worker
 
+  ALLOWED_DOMAIN = 'channel.org'.freeze
+
   def perform
     Rails.logger.info('Starting FedibuzzStreamWorker...')
-    FedibuzzStreamService.new.stream_from_fedi_buzz
+    if Rails.configuration.x.local_domain == ALLOWED_DOMAIN
+      FedibuzzStreamService.new.stream_from_fedi_buzz
+    else
+      Rails.logger.info("FedibuzzStreamWorker skipped: Domain does not match allowed domain.")
+    end
+
   rescue => e
     Rails.logger.error("FedibuzzStreamWorker encountered an error: #{e.message}")
   end
