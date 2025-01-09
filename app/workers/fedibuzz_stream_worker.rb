3f241e26ad1a0818adc2d46b87da5350074b3456
Aung Kyaw Phyo <kiru.kiru28@gmail.com>
2025-01-09 08:44:49 +0630

    Fix rubocop suggestions and delete un-used rake tasks

--- app/workers/fedibuzz_stream_worker.rb
+++ app/workers/fedibuzz_stream_worker.rb
@@ -8,9 +8,8 @@ class FedibuzzStreamWorker
     if Rails.configuration.x.local_domain == ALLOWED_DOMAIN
       FedibuzzStreamService.new.stream_from_fedi_buzz
     else
-      Rails.logger.info("FedibuzzStreamWorker skipped: Domain does not match allowed domain.")
+      Rails.logger.info('FedibuzzStreamWorker skipped: Domain does not match allowed domain.')
     end
-
   rescue => e
     Rails.logger.error("FedibuzzStreamWorker encountered an error: #{e.message}")
   end
