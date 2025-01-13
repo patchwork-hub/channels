3459068a88dcbfa8f57b5d7c47166d45862261ea
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2025-01-13 11:51:03 +0630

    Update tokens_controller.rb

--- app/controllers/oauth/tokens_controller.rb
+++ app/controllers/oauth/tokens_controller.rb
@@ -1,26 +1,21 @@
 # frozen_string_literal: true
 
 class Oauth::TokensController < Doorkeeper::TokensController
-  def create
-    if params[:username].blank? || params[:password].blank?
-      render json: { error: 'Username and password are required' }, status: 422
-      return
-    end
+  # def create
+  #   # You can add your own authentication logic here
+  #   if login_from_channel?
+  #     # Proceed with token generation
+  #     super
+  #   else
+  #     render json: { error: 'Record not found' }, status: 404
+  #   end
+  # end
 
-    # You can add your own authentication logic here
-    if login_from_channel?
-      # Proceed with token generation
-      super
-    else
-      render json: { error: 'Record not found' }, status: 404
-    end
-  end
+  # def revoke
+  #   unsubscribe_for_token if token.present? && authorized? && token.accessible?
 
-  def revoke
-    unsubscribe_for_token if token.present? && authorized? && token.accessible?
-
-    super
-  end
+  #   super
+  # end
 
   private
 
@@ -29,21 +24,24 @@ class Oauth::TokensController < Doorkeeper::TokensController
   end
 
   def login_from_channel?
-    user = User.find_by(email: params[:username])
+    if params[:grant_type].nil? ? false : params[:grant_type]
+      user = User.find_by(email: params[:username])
 
-    return true if (user.role.name == 'UserAdmin' || user.role&.id&.nil?) && is_create_channel_feed?
+      return true if (user.role.name == 'UserAdmin' || user.role&.id&.nil?) && is_create_channel_feed?
 
-    # If the user role is a UserAdmin || nil, there will have custom logic to sign in
-    if user.role.name == 'UserAdmin' || user.role&.id&.nil?
-      community_admin = CommunityAdmin.find_by(account_id: user&.account_id, role: 'UserAdmin', is_boost_bot: true)
-      return false if community_admin.nil?
+      # If the user role is a UserAdmin || nil, there will have custom logic to sign in
+      if user.role.name == 'UserAdmin' || user.role&.id&.nil?
+        community_admin = CommunityAdmin.find_by(account_id: user&.account_id, role: 'UserAdmin', is_boost_bot: true)
+        return false if community_admin.nil?
 
-      community = Community.find_by(id: community_admin&.patchwork_community_id)
-      return false if community.nil?
+        community = Community.find_by(id: community_admin&.patchwork_community_id)
+        return false if community.nil?
 
+        true
+      end
+    else
       true
     end
-    true
   end
 
   def is_create_channel_feed?
