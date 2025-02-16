17358688dcad7b5fd9e1f6d331f2962d7610be94
Aung Kyaw Phyo <kiru.kiru28@gmail.com>
2025-02-16 20:45:52 +0630

    Rollback features

--- app/lib/activitypub/activity/announce.rb
+++ app/lib/activitypub/activity/announce.rb
@@ -62,7 +62,7 @@ class ActivityPub::Activity::Announce < ActivityPub::Activity
 
   def distribute
     # Notify the author of the original status if that status is local
-    # LocalNotificationWorker.perform_async(@status.reblog.account_id, @status.id, 'Status', 'reblog') if reblog_of_local_account?(@status) && !reblog_by_following_group_account?(@status)
+    LocalNotificationWorker.perform_async(@status.reblog.account_id, @status.id, 'Status', 'reblog') if reblog_of_local_account?(@status) && !reblog_by_following_group_account?(@status)
 
     # Distribute into home and list feeds
     ::DistributionWorker.perform_async(@status.id) if @options[:override_timestamps] || @status.within_realtime_window?
