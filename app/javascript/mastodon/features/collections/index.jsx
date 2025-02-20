659355eea5902721e570e0b9e524ae5f69f2c5f4
soomyinthtel <gretaoto44@gmail.com>
2025-02-20 10:56:11 +0630

    fix: add loading indicator and no channels found.

--- app/javascript/mastodon/features/collections/index.jsx
+++ app/javascript/mastodon/features/collections/index.jsx
@@ -5,17 +5,19 @@ import { useDispatch, useSelector } from 'react-redux';
 import ChannelSearch from '../channel_search';
 import ChannelCard from 'mastodon/components/channel_card';
 import CollectionCard from 'mastodon/components/collection_card';
+import { LoadingIndicator } from 'mastodon/components/loading_indicator';
 
 const Collections = () => {
 
   const dispatch = useDispatch();
   const [searchTerm, setSearchTerm] = useState('');
 
-  const recommendedChannels = useSelector(state => state.recommended_channels.get('items'));
+  const collections = useSelector(state => state.recommended_channels.get('items'));
+  const collectionsLoading = useSelector(state => state.recommended_channels.get('isLoading'));
   const searchChannels = useSelector(state => state.search_channels.get('items'));
   const searchChannelsLoading = useSelector(state => state.search_channels.get('isLoading'));
 
-  const channels = searchTerm ? searchChannels : recommendedChannels;
+  const channels = searchTerm ? searchChannels : collections;
 
 
   const handleSearch = (term) => {
@@ -28,10 +30,10 @@ const Collections = () => {
   };
 
   useEffect(() => {
-    if (!searchTerm && recommendedChannels.size === 0) {
+    if (!searchTerm && collections.size === 0) {
       dispatch(fetchChannels());
     }
-  }, [searchTerm, recommendedChannels.size, dispatch]);
+  }, [searchTerm, collections.size, dispatch]);
 
   return (
     <div className='channels'>
@@ -39,39 +41,25 @@ const Collections = () => {
         <h2 className='title'>Explore channels</h2>
         <ChannelSearch  onSearch={handleSearch} isLoading={searchChannelsLoading}/>
       </div>
-      <div className='channels__list'>
-        {/* {channels.map((channel, index) => {
-          const isChannel = channel.type === 'channel';
-          const count = isChannel ? channel.attributes?.follower : channel.attributes?.community_count;
-          const label = isChannel
-            ? pluralize(count, 'follower', 'followers')
-            : pluralize(count, 'Channel', 'Channels');
-        return(
-          <a key={index} target='_blank' href={'https://' + channel.attributes.domain_name +'/public'}>
-            <div className='card'>
-              <img
-                src={channel.attributes.avatar_image_url}
-                alt={channel.attributes.name}
-                className='image' />
-              <div className='overlay' />
-              <div className='info'>
-                <p className='info__detail'>
-                  <span className='title'>{channel.attributes.name}</span>
-                  <span className='subtitle'>
-                      {formatNumber(count)} {label}
-                    </span>                
-                </p>
-                <Icon icon={ArrowRightUpAltIcon} id={''} className='icon' />
-              </div>
-            </div>
-          </a>
-        )})} */}
-        {channels.map((channel, index) => (
-          channel.type === 'channel' ?
-            <ChannelCard key={index} channel={channel} /> :
-            <CollectionCard key={index} channel={channel} />
-        ))}
-      </div>
+      {searchChannelsLoading || collectionsLoading ? (
+        <div className='channels__loading'>
+          <LoadingIndicator />
+        </div>
+      ) : channels.size === 0 ? (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '60vh' }}>
+          <p style={{ fontSize:"20px"}}>No channels found.</p>
+        </div>
+      ) : (
+        <div className='channels__list'>
+          {channels.map((channel, index) => (
+            channel.type === 'channel' ? (
+              <ChannelCard key={index} channel={channel} />
+            ) : (
+              <CollectionCard key={index} channel={channel} />
+            )
+          ))}
+        </div>
+      )}
     </div>
   );
 };
