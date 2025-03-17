790f60b404d79ca62dc1928d2265f176e10703a7
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2025-03-17 16:27:38 +0630

    feat: enhance scheduled status update with improved parameter handling and error management

--- app/controllers/api/v1/scheduled_statuses_controller.rb
+++ app/controllers/api/v1/scheduled_statuses_controller.rb
@@ -21,8 +21,12 @@ class Api::V1::ScheduledStatusesController < Api::BaseController
   end
 
   def update
-    @status.update!(scheduled_status_params)
+    @status.destroy!
+    @status = post_status_service
+
     render json: @status, serializer: REST::ScheduledStatusSerializer
+  rescue PostStatusService::UnexpectedMentionsError => e
+    render json: unexpected_accounts_error_json(e), status: 422
   end
 
   def destroy
@@ -41,7 +45,30 @@ class Api::V1::ScheduledStatusesController < Api::BaseController
   end
 
   def scheduled_status_params
-    params.permit(:scheduled_at)
+    # params.permit(:scheduled_at) # Original Code
+    params.permit(
+      :status,
+      :in_reply_to_id,
+      :sensitive,
+      :spoiler_text,
+      :visibility,
+      :language,
+      :scheduled_at,
+      allowed_mentions: [],
+      media_ids: [],
+      media_attributes: [
+        :id,
+        :thumbnail,
+        :description,
+        :focus,
+      ],
+      poll: [
+        :multiple,
+        :hide_totals,
+        :expires_in,
+        options: [],
+      ]
+    )
   end
 
   def next_path
@@ -58,5 +85,25 @@ class Api::V1::ScheduledStatusesController < Api::BaseController
 
   def pagination_collection
     @statuses
+  end
+
+  def post_status_service
+    PostStatusService.new.call(
+      current_user.account,
+      text: scheduled_status_params[:status],
+      thread: nil,
+      media_ids: scheduled_status_params[:media_ids],
+      sensitive: scheduled_status_params[:sensitive],
+      spoiler_text: scheduled_status_params[:spoiler_text],
+      visibility: scheduled_status_params[:visibility],
+      language: scheduled_status_params[:language],
+      scheduled_at: scheduled_status_params[:scheduled_at],
+      drafted: false,
+      application: doorkeeper_token.application,
+      poll: scheduled_status_params[:poll],
+      allowed_mentions: scheduled_status_params[:allowed_mentions],
+      idempotency: request.headers['Idempotency-Key'],
+      with_rate_limit: true,
+    )
   end
 end
