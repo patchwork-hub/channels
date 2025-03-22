c5b160fecd355bbab5d152d58ec9727e93bbb5a1
Aung Kyaw Phyo <kiru.kiru28@gmail.com>
2025-03-22 22:13:48 +0700

    Return only Create AP type

--- app/presenters/activitypub/activity_presenter.rb
+++ app/presenters/activitypub/activity_presenter.rb
@@ -7,8 +7,9 @@ class ActivityPub::ActivityPresenter < ActiveModelSerializers::Model
     def from_status(status, allow_inlining: true)
       new.tap do |presenter|
         presenter.id        = ActivityPub::TagManager.instance.activity_uri_for(status)
-        presenter.type      = status.reblog? ? 'Announce' : 'Create'
-        presenter.actor     = '' # ActivityPub::TagManager.instance.uri_for(status.account)
+        # presenter.type      = status.reblog? ? 'Announce' : 'Create'
+        presenter.type      = 'Create'
+        presenter.actor     = ActivityPub::TagManager.instance.uri_for(status.account)
         presenter.published = status.created_at
         presenter.to        = ActivityPub::TagManager.instance.to(status)
         presenter.cc        = ActivityPub::TagManager.instance.cc(status)
