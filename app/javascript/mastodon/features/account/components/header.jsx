95acb5c2028882645f51b10f0b9da52f88e1216b
Chan Nyein Tun <29679269+channyeintun@users.noreply.github.com>
2025-03-24 10:28:47 +0700

    Revert "fix: add line break for mandatory text"
    This reverts commit df263c0ba61097fd276d42ecaeb608af3ceb8fbf.

--- app/javascript/mastodon/features/account/components/header.jsx
+++ app/javascript/mastodon/features/account/components/header.jsx
@@ -400,7 +400,7 @@ class Header extends ImmutablePureComponent {
       }
     }
 
-    const content         = { __html: account.get('note_emojified').replace("[[BR]]","<br />") };
+    const content         = { __html: account.get('note_emojified') };
     const displayNameHtml = { __html: account.get('display_name_html') };
     const fields          = account.get('fields');
     const isLocal         = account.get('acct').indexOf('@') === -1;
@@ -468,7 +468,7 @@ class Header extends ImmutablePureComponent {
               <div className='account__header__bio' ref={this.setRef}>
                 {(account.get('id') !== me && signedIn) && <AccountNoteContainer account={account} />}
 
-                {account.get('note').length > 0 && account.get('note') !== '<p></p>' && <div className='account__header__content translate' dangerouslySetInnerHTML={content} />}
+                {account.get('note').length > 0 && account.get('note') !== '<p></p>' && <div className='account__header__content translate' dangerouslySetInnerHTML={content.replace("[[BR]]","<br />")} />}
 
                 <div className='account__header__fields'>
                   <dl>
--- app/javascript/mastodon/features/follow_requests/components/account_authorize.jsx
+++ app/javascript/mastodon/features/follow_requests/components/account_authorize.jsx
@@ -30,7 +30,7 @@ class AccountAuthorize extends ImmutablePureComponent {
 
   render () {
     const { intl, account, onAuthorize, onReject } = this.props;
-    const content = { __html: account.get('note_emojified').replace("[[BR]]","<br />") };
+    const content = { __html: account.get('note_emojified') };
 
     return (
       <div className='account-authorize__wrapper'>
@@ -40,7 +40,7 @@ class AccountAuthorize extends ImmutablePureComponent {
             <DisplayName account={account} />
           </Link>
 
-          <div className='account__header__content translate' dangerouslySetInnerHTML={content} />
+          <div className='account__header__content translate' dangerouslySetInnerHTML={content.replace("[[BR]]","<br />")} />
         </div>
 
         <div className='account--panel'>
