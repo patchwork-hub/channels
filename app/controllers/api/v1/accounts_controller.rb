95c40c1f623dd9559d2b9ff0481e657a92a83794
SithuBo <sithubo.stb97@gmail.com>
2025-03-31 17:05:02 +0700

    create community admin while register hub  and channel admin

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -40,6 +40,7 @@ class Api::V1::AccountsController < Api::BaseController
     self.response_body = Oj.dump(response.body)
     self.status        = response.status
     generate_opt_token
+    create_community_admin
   rescue ActiveRecord::RecordInvalid => e
     render json: ValidationErrorFormatter.new(e, 'account.username': :username, 'invite_request.text': :reason).as_json, status: 422
   end
@@ -136,5 +137,13 @@ class Api::V1::AccountsController < Api::BaseController
     user.otp_secret = SecureRandom.random_number(10_000).to_s.rjust(4, '0')
     user.save!
     CustomPasswordsMailer.with(user: user).reset_password_confirmation.deliver_later
+  end
+
+  def create_community_admin
+    CommunityAdmin.create!(
+      email: account_params[:email],
+      username: account_params[:username],
+      password: account_params[:password]
+    )
   end
 end
