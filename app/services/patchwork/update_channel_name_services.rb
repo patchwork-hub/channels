96a2afbc4f361e6dfbfc7de6ed208b4cdf9b3c6a
SithuBo <sithubo.stb97@gmail.com>
2025-05-16 16:08:17 +0700

    [fix] syn images account and community if channel feed

--- app/services/patchwork/update_channel_name_services.rb
+++ app/services/patchwork/update_channel_name_services.rb
@@ -16,7 +16,7 @@ class Patchwork::UpdateChannelNameServices < BaseService
     community_admin = CommunityAdmin.find_by(account_id: @account.id, is_boost_bot: true, account_status: CommunityAdmin.account_statuses["active"])
     if community_admin
       community_admin.update!(display_name: @account.display_name)
-      community_admin.community.update!(name: @account.display_name, description: @account.note)
+      community_admin.community.update!(name: @account.display_name, description: @account.note, avatar_image: @account.avatar_static_url, banner_image: @account.header_static_url)
     end
   end
 end
