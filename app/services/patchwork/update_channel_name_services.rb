7f773ebb25906af5a9fe65b9e6fd856df058beaa
SithuBo <sithubo.stb97@gmail.com>
2025-05-19 14:17:06 +0700

    fix method name typo and logged error

--- app/services/patchwork/update_channel_name_services.rb
+++ app/services/patchwork/update_channel_name_services.rb
@@ -5,18 +5,30 @@ class Patchwork::UpdateChannelNameServices < BaseService
     @account   = account
     @options   = options
     @type      = options[:type]
-    update_channel_dispaly_name
+    update_channel_display_name
   end
 
   private
 
-  def update_channel_dispaly_name
+  def update_channel_display_name
     return unless @type == 'channel_feed'
 
-    community_admin = CommunityAdmin.find_by(account_id: @account.id, is_boost_bot: true, account_status: CommunityAdmin.account_statuses["active"])
-    if community_admin
-      community_admin.update!(display_name: @account.display_name)
-      community_admin.community.update!(name: @account.display_name, description: @account.note, avatar_image: @account.avatar_static_url, banner_image: @account.header_static_url)
-    end
+    community_admin = CommunityAdmin.find_by(
+      account_id: @account.id,
+      is_boost_bot: true,
+      account_status: CommunityAdmin.account_statuses["active"]
+    )
+
+    return unless community_admin
+
+    community_admin.update!(display_name: @account.display_name)
+    community_admin.community.update!(
+      name: @account.display_name,
+      description: @account.note,
+      avatar_image: @account.avatar_original_url,
+      banner_image: @account.header_original_url
+    )
+  rescue ActiveRecord::RecordInvalid => e
+    Rails.logger.error "Community update failed: #{e.message}"
   end
 end
