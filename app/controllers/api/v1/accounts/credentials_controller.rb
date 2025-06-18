342dd9d8935113c537155ab477f40ef4c40c8d53
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2025-06-18 10:31:28 +0630

    checking main channel

--- app/controllers/api/v1/accounts/credentials_controller.rb
+++ app/controllers/api/v1/accounts/credentials_controller.rb
@@ -15,7 +15,7 @@ class Api::V1::Accounts::CredentialsController < Api::BaseController
     UpdateAccountService.new.call(@account, account_params, raise_error: true)
     current_user.update(user_params) if user_params
     ActivityPub::UpdateDistributionWorker.perform_async(@account.id)
-    Patchwork::UpdateChannelNameServices.new.call(@account, type: 'channel_feed')
+    Patchwork::UpdateChannelNameServices.new.call(@account, type: 'channel_feed') if main_channel?
     render json: @account, serializer: REST::CredentialAccountSerializer
   rescue ActiveRecord::RecordInvalid => e
     render json: ValidationErrorFormatter.new(e).as_json, status: 422
