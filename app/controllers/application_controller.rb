b82180afeabf257be166c1443b16723cdfbde97f
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2025-06-18 13:00:56 +0630

    refactor main channel type

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,7 @@ class ApplicationController < ActionController::Base
   include DatabaseHelper
   include AuthorizedFetchHelper
   include SelfDestructHelper
+  include ChannelHelper
 
   helper_method :current_account
   helper_method :current_session
@@ -184,7 +185,4 @@ class ApplicationController < ActionController::Base
     response.cache_control.replace(private: true, no_store: true)
   end
 
-  def main_channel?
-    ENV.fetch('MAIN_CHANNEL', nil) != nil  && ENV.fetch('MAIN_CHANNEL', nil) != 'false'
-  end
 end
