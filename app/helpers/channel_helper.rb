93faad2988aace7e60638584a2dff255a78002b7
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2025-06-20 16:57:55 +0630

    modify:orginisation admin login

--- app/helpers/channel_helper.rb
+++ app/helpers/channel_helper.rb
@@ -2,6 +2,9 @@
 
 module ChannelHelper
   def main_channel?
+    if Rails.env.local? || Rails.env.test?
+      return true
+    end
     ENV.fetch('MAIN_CHANNEL', nil) != nil && ENV.fetch('MAIN_CHANNEL', nil) != 'false'
   end
 end
