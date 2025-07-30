58ad4a87b3ceb1ba0de991be503ead42509cd7fe
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2025-07-30 23:43:03 +0630

    fix:banned statuses

--- app/chewy/public_statuses_index.rb
+++ app/chewy/public_statuses_index.rb
@@ -55,6 +55,7 @@ class PublicStatusesIndex < Chewy::Index
   index_scope ::Status.unscoped
                       .kept
                       .indexable
+                      .without_banned
                       .includes(:media_attachments, :preloadable_poll, :tags, preview_cards_status: :preview_card)
 
   root date_detection: false do
