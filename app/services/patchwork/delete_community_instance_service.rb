7b8ec280795dfe4277d4a136c080aa0aa539448c
Nyan Lin Htut <nyanlinhtut925@gmail.com>
2025-11-25 14:11:10 +0700

    feat: implement DeleteCommunityInstanceService integrate lambda for community deletion

---
+++ app/services/patchwork/delete_community_instance_service.rb
@@ -0,0 +1,59 @@
+require 'httparty'
+
+class DeleteCommunityInstanceService < BaseService
+  LAMBDA_URL = ENV.fetch('DELETE_COMMUNITY_LAMBDA_URL', nil)
+  LAMBDA_API_KEY = ENV.fetch('DELETE_COMMUNITY_LAMBDA_API_KEY', nil)
+
+  def call(community_id)
+    return false if LAMBDA_URL.nil? || LAMBDA_API_KEY.nil?
+
+    return false if community.ip_address_id.nil?
+
+    ip_address = IpAddress.find(com.ip_address_id)
+    return false if ip_address.nil?
+
+    ip = ip_address&.ip
+
+    @payload = {
+      client: "#{community_id}_#{community.slug}",
+      ip_address: ip,
+    }
+    response = invoke_lambda
+    if response.success?
+      decrement_use_count(ip_address)
+      true
+    else
+      log_failed_response(response)
+      false
+    end
+  rescue => e
+    Rails.logger.error "[DeleteCommunityInstanceService] Error calling lambda: #{e.message}"
+    Rails.logger.error "[DeleteCommunityInstanceService] Backtrace: #{e.backtrace.join("\n")}"
+    false
+  end
+
+  private
+
+  def invoke_lambda
+    HTTParty.post(
+      LAMBDA_URL,
+      body: @payload,
+      headers: {
+        'Content-Type' => 'application/json',
+        'x-api-key' => LAMBDA_API_KEY,
+      }
+    )
+  end
+
+  def decrement_use_count(ip_address)
+    ip_address.update!(use_count: ip_address.use_count - 1)
+  rescue => e
+    Rails.logger.error "[DeleteCommunityInstanceService] Error decrementing use_count: #{e.message}"
+    raise
+  end
+
+  def log_failed_response(response)
+    Rails.logger.error "[DeleteCommunityInstanceService] Lambda call failed with status: #{response.code}"
+    Rails.logger.error "[DeleteCommunityInstanceService] Response body: #{response.body}"
+  end
+end
--- app/workers/community_cleanup_worker.rb
+++ app/workers/community_cleanup_worker.rb
@@ -6,7 +6,7 @@ class CommunityCleanupWorker
                            .where('deleted_at <= ?', 30.days.ago)
 
     if communities.empty?
-      Rails.logger.info "[CommunityCleanupWorker] No communities to clean up."
+      Rails.logger.info '[CommunityCleanupWorker] No communities to clean up.'
       return
     end
 
@@ -15,21 +15,29 @@ class CommunityCleanupWorker
     communities.find_each do |community|
       begin
         ActiveRecord::Base.transaction do
-          account_ids = community&.community_admins.pluck(:account_id)
+          account_ids = community&.community_admins&.pluck(:account_id)
 
           Rails.logger.info "[CommunityCleanupWorker] Deleting community ##{community.id}..."
-          community.destroy
 
-          account_ids.compact.uniq.each do |account_id|
-            if Account.exists?(account_id)
-              Admin::AccountDeletionWorker.perform_async(account_id, {
-                'reserve_username' => false,
-                'reserve_email' => false
-              })
-              Rails.logger.info "[CommunityCleanupWorker] Enqueued deletion for account ##{account_id}."
-            else
-              Rails.logger.warn "[CommunityCleanupWorker] Account ##{account_id} not found. Skipping deletion."
+          response = Patchwork::DeleteCommunityInstanceService.new.call(community.id)
+
+          if response
+            Rails.logger.info "[CommunityCleanupWorker] Successfully called DeleteCommunityInstanceService for community ##{community.id}."
+            community.destroy
+
+            account_ids.compact.uniq.each do |account_id|
+              if Account.exists?(account_id)
+                Admin::AccountDeletionWorker.perform_async(account_id, {
+                  'reserve_username' => false,
+                  'reserve_email' => false,
+                })
+                Rails.logger.info "[CommunityCleanupWorker] Enqueued deletion for account ##{account_id}."
+              else
+                Rails.logger.warn "[CommunityCleanupWorker] Account ##{account_id} not found. Skipping deletion."
+              end
             end
+          else
+            Rails.logger.warn "[CommunityCleanupWorker] Skipping destroy for community ##{community.id} due to service call failure."
           end
           sleep(0.05)
         end
@@ -38,6 +46,6 @@ class CommunityCleanupWorker
       end
     end
 
-    Rails.logger.info "[CommunityCleanupWorker] Cleanup completed."
+    Rails.logger.info '[CommunityCleanupWorker] Cleanup completed.'
   end
 end
