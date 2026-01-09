79956df67d6d96d96f3cd3efbc674a36a84eb6a6
soemyinthtel <soemyinthtel.github@gmail.com>
2026-01-09 23:45:41 +0630

    fix: topBanner aspectRatio
    - Centre timeline top banner aspectRation fix as !important

--- app/javascript/mastodon/components/channel_top_banner.tsx
+++ app/javascript/mastodon/components/channel_top_banner.tsx
@@ -7,11 +7,10 @@ const ChannelTopBanner = () => {
         display: 'flex',
         flexDirection: 'column-reverse',
         width: '100%',
-        aspectRatio: '1.96',
+        aspectRatio: '1.96 !important',
         background: `linear-gradient(180deg, rgba(43, 43, 43, 0.00) 0%, rgba(37, 37, 37, 0.60) 56.93%), url(${header_image}) lightgray 50% / cover no-repeat`,
       }}
-    >
-    </div>
+     />
   );
 };
 
